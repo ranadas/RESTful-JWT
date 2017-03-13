f3bccef9581e1ecbac98f2b8d50dc2b9bdee56ed
x148128 <rana.das@hrblock.com>
2017-03-13 15:53:19 +0000

    Adding a JpaRepository Test

---
+++ src/test/java/com/rdas/repository/PersonRepositoryTest.java
@@ -0,0 +1,34 @@
+package com.rdas.repository;
+
+import com.rdas.ApplicationMain;
+import com.rdas.model.Person;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+/**
+ * Created by x148128 on 13/03/2017.
+ */
+@ContextConfiguration(classes={ApplicationMain.class})
+@ActiveProfiles("test")
+@Transactional
+@RunWith(SpringJUnit4ClassRunner.class)
+public class PersonRepositoryTest {
+    @Autowired
+    private PersonRepository personRepository;
+
+    @Test
+    public void testThatAKnownUserCanBeSearched(){
+        Person personByName = personRepository.findByName("Zack Murphy");
+        assertThat(personByName, is(notNullValue()));
+        System.out.println(personByName.toString());
+    }
+}
